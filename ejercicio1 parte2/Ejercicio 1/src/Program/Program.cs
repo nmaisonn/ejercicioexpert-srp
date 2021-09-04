03a71509159c59314a5bfc14f68a6e4572bf0170
sebastianfori <you.com>
2021-09-04 12:55:33 -0300

    Ejercicio2-parte2

---
+++ ejercicio1 parte2/Ejercicio 1/src/Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SRP
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca bibliotecaUCU = new Biblioteca();
+            Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034");
+            Libro libro2 = new Libro("Pro C#","Troelsen","001-035");
+
+            bibliotecaUCU.AgregarLibro(libro1, "A", "7");
+            bibliotecaUCU.AgregarLibro(libro2, "B", "3");
+
+            Console.WriteLine(bibliotecaUCU.Estantes[1].Libros[0].Titulo);
+        }
+    }
+}
